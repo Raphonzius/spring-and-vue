9ec9470bd7b3a8fd82dc55d5a2ba00f6f6b83e70
r.pereira <r.pereira@samsung.com>
2024-09-19 08:53:40 -0300

    CorsMapping Updated to allow any origin

--- src/main/java/io/github/raphonzius/backend_spring/core/config/SecurityConfig.java
+++ src/main/java/io/github/raphonzius/backend_spring/core/config/SecurityConfig.java
@@ -26,7 +26,7 @@ public class SecurityConfig implements WebMvcConfigurer{
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
-                .allowedOrigins("http://localhost:8081")
+                .allowedOrigins("*")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
